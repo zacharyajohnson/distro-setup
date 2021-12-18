bd6be7e84b1cc89a6d68a65ed320bf300b93d081
Zachary Alan Johnson <zjohnson366@gmail.com>
2021-12-18 18:03:41 -0500

    Remove <<< operator and unneeded single quotes
    Remove bash specific operator, the <<< operator to make
    it more portable. Remove uneeded single quotes when exporting
    PATH variable

--- setup-distro.sh
+++ setup-distro.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 sudo apt-get update && sudo apt-get upgrade
 
@@ -38,13 +38,13 @@ echo "creating $HOME/bin" && sudo mkdir -p $HOME/bin && \
 	#but the updated PATH variable exists in the .bash_profile file itself. We just wipe it clean each time and start over. So we only have to check if the folder is on the current PATH itself.
 	#If the bin folder for some reason exists on the PATH, we just use $PATH in our export statment instead of adding it, otherwise we add $HOME/bin to the path.
 	#This effectivly handles the case if the script is run multiple times as well
-	if grep -q $HOME/bin <<< $PATH 
+	if echo "$PATH" | grep -q $HOME/bin
 	then
 		echo "$HOME/bin already on path, using current PATH variable in .bash_profile" && sleep 2s
-		echo "export PATH='$PATH'" | sudo tee -a $HOME/.bash_profile
+		echo "export PATH=$PATH" | sudo tee -a $HOME/.bash_profile
 	else
 		echo "adding $HOME/bin to PATH variable..." && echo "Please login again to see PATH variable changes!" && sleep 2s
-	        echo "export PATH='$HOME/bin:$PATH'" | sudo tee -a $HOME/.bash_profile 
+	        echo "export PATH=$HOME/bin:$PATH" | sudo tee -a $HOME/.bash_profile
 	fi
 
 # Move config files for custom tmux setups to the bin folder so they can be executed anywhere
