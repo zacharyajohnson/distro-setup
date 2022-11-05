3a6d22e14ef95ca990422310111a370d984c6eeb
Zachary Alan Johnson <zjohnson366@gmail.com>
2022-11-05 12:10:24 -0400

    Fix _dir_files_total_line_count to work with bourne shell
    The dir files method was failing in actual bourne shell
    implementations(such as FreeBSD) because string
    indexing is undefined in POSIX sh(see https://www.shellcheck.net/wiki/SC3057)
    
    This one fix caused me to have to restructure the entire function, but
    it turned out for the best because now the function is a lot less
    complex

--- shell/common/.aliases.sh
+++ shell/common/.aliases.sh
@@ -5,44 +5,25 @@ _git_all_commits() {
 }
 
 _dir_files_total_line_count() {
-        file_search_max_depth=1
-        parameter_offset=1
-
-
-        if [ "$1" = "-maxdepth" ]; then
-                file_search_max_depth="$2"
-
-                parameter_offset=3
-        fi
-
-        cmd="find . -maxdepth $file_search_max_depth -type f"
-
-        arg_total_count="$#"
-        arg_count="$parameter_offset"
+        cmd='find . -type f -path "*"'
 
         first_loop=true
-        for reg_expression in "${@:$parameter_offset}"
+        for reg_expression in "$@"
         do
                 if [ "$first_loop" = true ]; then
-                        cmd=$(printf "%s \\( " "$cmd")
+                        cmd=$(printf "%s | grep" "$cmd")
                         first_loop=false
                 fi
 
-                if [ "$arg_count" -eq "$arg_total_count" ]; then
-                        if [ -z "$reg_expression" ]; then
-                                reg_expression="*"
-                        fi
-                        cmd=$(printf "%s -iname \"%s\"" "$cmd" "$reg_expression")
-                        if [ -n "$3" ] || [ $parameter_offset -eq 1 ]; then
-                                cmd=$(printf "%s \\)" "$cmd")
-                        fi
-                else
-                        cmd=$(printf "%s -iname \"%s\" -o" "$cmd" "$reg_expression")
-                        arg_count=$((arg_count+1))
+                if [ -z "$reg_expression" ]; then
+                        reg_expression="*"
                 fi
+
+                cmd=$(printf "%s -e \\$reg_expression\$" "$cmd")
         done
-      
-        cmd=$(printf "%s -print0 | xargs -0 wc -l" "$cmd" | sed -e 's/,/ /g')
+
+        cmd=$(printf "%s | xargs wc -l" "$cmd" | sed -e 's/,/ /g')
+
         eval "$cmd"
 }
 
