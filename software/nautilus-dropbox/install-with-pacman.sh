151c1b4b66881cb6a92966212c3c733bdfc646b7
Zachary Alan Johnson <zjohnson366@gmail.com>
2023-05-22 02:04:47 -0400

    Fix dropbox/qdirstat for pacman install
    Dropbox/qdirstat are part of the AUR repo.
    Bascially, a community run first of the offical
    arch repo. To pull from the AUR, you need
    to pull down the tar file, extract it,
    and run the makepkg command to install it
    via pacman. Altered their scripts to do just
    that

--- software/nautilus-dropbox/install-with-pacman.sh
+++ software/nautilus-dropbox/install-with-pacman.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 # Python-gpgme is a hard dependency for dropbox in arch for some reason
-sudo pacman -S python-gpgme dropbox
+sudo pacman -S python-gpgme
+
+curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/dropbox.tar.gz
+tar -xvf dropbox.tar.gz
+rm -rf dropbox.tar.gz
+
+cd dropbox/
+makepkg -si
+
+cd ../
+rm -rf dropbox/
--- software/qdirstat/install-with-pacman.sh
+++ software/qdirstat/install-with-pacman.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 sudo pacman -S qdirstat
+
+curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/qdirstat.tar.gz
+
+tar -xvf qdirstat.tar.gz
+rm -rf qdirstat.tar.gz
+
+cd qdirstat
+makepkg -si qdirstat
+
+cd ../
+rm -rf qdirstat
