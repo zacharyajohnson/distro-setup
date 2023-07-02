5180f18ea9a711f9a51cd8b6137a0fad88d0f9af
Zachary Alan Johnson <zjohnson366@gmail.com>
2023-07-02 03:14:04 -0400

    Add install script for Anki(no package manager)

---
+++ software/anki/install-with-no-package-manager.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+version='2.1.65'
+file_name="anki-$version-linux-qt6"
+zipped_file_name="$file_name.tar.zst"
+
+curl -OL "https://github.com/ankitects/anki/releases/download/$version/$zipped_file_name"
+tar --use-compress-program=unzstd -xvf "$zipped_file_name"
+
+(
+        cd "$file_name" || exit
+        sudo ./install.sh
+)
+
+rm -rf "$file_name"
+rm -rf "$zipped_file_name"
