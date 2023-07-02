2cdbd600ac2f7f53eac6d68a54deb645ce37811b
Zachary Alan Johnson <zjohnson366@gmail.com>
2023-07-02 03:30:47 -0400

    Add install script for intellij ultimate/community(flatpak)
    This script will prompt the user to choose which version of
    Intellij the user wants to install, which is either the
    ultimate or community version. Both versions of Intellij
    are installed using flatpak

---
+++ software/intellij/install-with-flatpak.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+printf 'Would you like to install:\n    (1/Default)IntelliJ Community\n    (2)IntelliJ Ultimate\n'
+
+read -r answer
+
+if [ -z "$answer" ] || [ "$answer" -eq 1 ]; then
+        printf 'Installing IntelliJ Community\n'
+        sudo flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
+elif [ "$answer" -eq 2 ]; then
+        printf 'Installing Intellij Ultimate\n'
+        sudo flatpak install flathub com.jetbrains.IntelliJ-IDEA-Ultimate
+else
+        printf 'Invalid option. Aborting...\n'
+        exit
+fi
+
