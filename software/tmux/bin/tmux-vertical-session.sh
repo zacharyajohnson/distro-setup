e9ffa05c2c9275760accff9fcc25980b5998fe99
Zachary Alan Johnson <zjohnson366@gmail.com>
2023-09-01 20:26:49 -0400

    Change tmux-vertical-session.sh to suport n commands

--- software/tmux/bin/tmux-vertical-session.sh
+++ software/tmux/bin/tmux-vertical-session.sh
@@ -1,27 +1,57 @@
 #!/bin/sh
 
-# $1 - The first command that should be exected.
-# Appears on the leftmost pane
+# This script is used to create a number of vertical
+# panes in a tmux session. 
 
-# $2 - The second command that should be executed.
-# Appears on the rightmost pane
+# The script will take any number of commands as 
+# its arguments, create a vertical pane for each command, 
+# and execute that command.
 
-# Create a tmux session with name vertical-session
-tmux new-session -d -s vertical-session -n main-window
+# If no commands are passed in, tmux will create
+# two vertical panes that start a shell. 
 
-# Set first pane to be called after the first command
-tmux select-pane -t 0 -T "$1"
+# If one command is passed in tmux will execute the command 
+# in one pane and the other will be a pane that starts a shell.
 
-# Create a vertical window to the right for the second command
-tmux split-window -h
-tmux select-pane -t 1 -T "$2"
+window_name='main-window'
+session_name='vert-sess'
+num_commands="$#"
 
-# Send the commands to both windows to execute them
-tmux send-keys -t 'vertical-session:main-window.0' "$1" C-m
-tmux send-keys -t 'vertical-session:main-window.1' "$2" C-m
+# Create a tmux session with name vert-session.
+tmux new-session -d -s "$session_name" -n "$window_name"
 
-# Select our left most pane
+if [ "$num_commands" -eq 0 ]; then
+        tmux select-pane -t 0 -T ''
+
+        tmux split-window -h
+        tmux select-pane -t 1 -T ''
+elif [ "$num_commands" -eq 1 ]; then
+        tmux select-pane -t 0 -T "$1"
+
+        tmux split-window -h
+        tmux select-pane -t 1 -T ''
+
+        tmux send-keys -t "$session_name:$window_name.0" "$1" C-m
+else
+        for i in $(seq '1' "$num_commands"); do 
+                command_variable="\$$i"
+                pane_number=$((i - 1)) 
+                command_value=$(eval echo "$command_variable")
+
+                tmux select-pane -t "$pane_number" -T "$command_value"
+                tmux send-keys -t "$session_name:$window_name.$pane_number" "$command_value" C-m
+
+                # If i = num_commands, we are done and dont
+                # want to create an extra window.
+                if [ ! "$i" -eq "$num_commands" ]; then
+                        tmux split-window -h
+                fi
+
+        done
+fi
+
+# Select our left most pane.
 tmux select-pane -t 0
 
-# Attach to the vertical session now that configuration is done
-tmux attach-session -t vertical-session
+# Attach to the vertical session now that configuration is done.
+tmux attach-session -t "$session_name"
