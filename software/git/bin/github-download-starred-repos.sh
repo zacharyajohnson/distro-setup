84f3ce69ca23ec227d18f2084d5e616d2d2a6764
Zachary Alan Johnson <zjohnson366@gmail.com>
2023-09-09 23:42:26 -0400

    Move github scripts to git software package
    Created bin folder for git and added scripts to download
    personal repos/starred repos of a user

---
+++ software/git/bin/github-download-starred-repos.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+# This script will download all the starred repos
+# for a user based on the $GITHUB_USERNAME environment variable
+
+current_dir="$(pwd)"
+base_dir="$1"
+
+if [ -z "$base_dir" ]; then
+        echo "Provide folder path to store GitHub repos"
+        exit
+fi
+
+if [ -z "$GITHUB_USERNAME" ]; then
+        echo 'GITHUB_USERNAME environment variable not set'
+        exit
+fi
+
+if [ -z "$GITHUB_READ_TOKEN" ]; then
+        echo 'GITHUB_READ_TOKEN not set. Please provide read only api token for GitHub'
+        exit
+fi
+
+
+echo "Storing repos at $base_dir"
+echo "Downloading starred repos for user $GITHUB_USERNAME"
+
+github_starred_repos_endpoint="https://api.github.com/users/$GITHUB_USERNAME/starred?per_page=100"
+
+while [ -n "$github_starred_repos_endpoint" ]; do
+        # The full response
+        response="$(curl -s -H "Authorization: token $GITHUB_READ_TOKEN" "$github_starred_repos_endpoint")"
+
+        # The data we care about
+        response_data="$(echo "$response" | jq 'map({owner_username: .owner.login, repo_path: .full_name, clone_url: .clone_url})')"
+
+        echo "$response_data" | jq -c '.[]' | while read -r i; do
+               owner_username="$(echo "$i" | jq -r '.owner_username')"
+               repo_path="$(echo "$i" | jq -r '.repo_path')"
+               repo_url="$(echo "$i" | jq -r '.clone_url')"
+               repo_clone_path="$base_dir/$repo_path"
+
+
+               # GitHub in its infinite wisdom doesn't unstar a repo
+               # after it is DMCA claimed so it halts the program when
+               # trying to grab it
+               if [ "$repo_path" = 'rmonvfer/yandereSimulatorSource' ]; then
+                       echo "$repo_url has been DMCA claimed. Skipping..."
+                       continue
+               fi
+
+
+               if [ -d "$repo_clone_path" ]; then
+                       cd "$repo_clone_path"
+                       if git rev-parse --show-toplevel > /dev/null 2>&1; then
+                               echo "$repo_url exists in $repo_clone_path. Updating..."
+                               git pull
+                               cd "$current_dir"
+                               #sleep 10s
+                               continue
+                       else
+                               echo "Directory $repo_clone_path exists but isn't a git directory. Skipping..."
+                               #sleep 5s
+                               cd "$current_dir"
+                       fi
+               else
+                       mkdir -p "$repo_clone_path"
+                       cd "$current_dir"
+                       echo "Cloning $repo_url from user $owner_username into $repo_path"
+                       #sleep 10s
+
+                       git clone "$repo_url" "$repo_clone_path" > '/dev/null'
+               fi
+        done
+
+        github_starred_repos_endpoint="$(curl -sI -H "Authorization: token $GITHUB_READ_TOKEN" "$github_starred_repos_endpoint" | grep -i '^Link:' | sed -E -n 's/.*<([^>]+)>;[[:space:]]*rel="next".*/\1/p')"
+done
+
---
+++ software/git/install-bin-scripts.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export_dir="$HOME/.distro-config/bin"
+mkdir -p "$export_dir"
+cp -r bin/. "$export_dir"
