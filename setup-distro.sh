30e40089c39346c3168163444d094f53c945501c
Zachary Alan Johnson <zjohnson366@gmail.com>
2023-09-09 23:50:36 -0400

    Remove bin dir creation since this is configured in software dir

--- setup-distro.sh
+++ setup-distro.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-#Creating a bin directory at the current logged in users home and add it to the PATH variable so we can execute any scripts that are convenent to run anywhere
-echo "creating $HOME/bin" && mkdir -p "$HOME/bin"
-
 cd shell && ./setup.sh
 cd ../
 
