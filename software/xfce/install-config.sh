0f89db32d5fc0c1e0a1a15c2e93b79780df15508
Zachary Alan Johnson <zjohnson366@protonmail.com>
2024-03-08 22:49:35 -0500

    Add setup for xfce(pacman)
    Fucking KDE released version 6.0.0 and its a god damn mess.
    Everything broke and Wayland sucked ass as well.
    Moving to xfce until they get their shit together.

---
+++ software/xfce/install-config.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp 'config/.xinitrc' "$HOME"
---
+++ software/xfce/install-with-pacman.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo pacman -S xfce4
+sudo pacman -S xfce4-goodies
+sudo pacman -S network-manager-applet
+sudo pacman -S pulseaudio
+sudo pacman -S pavucontrol
