66071963156900410cec53602eb89849fb95c476
Zachary Alan Johnson <zjohnson366@protonmail.com>
2024-03-27 09:04:07 -0400

    Add check if HOME env var is set in common.config.sh

--- shell/common/common-config.sh
+++ shell/common/common-config.sh
@@ -8,50 +8,53 @@ _is_on_path() {
         return $?
 }
 
-bin_directory="$HOME/.distro-config/bin"
+if [ -z "$HOME" ]; then
+        echo "HOME environment variable is not set"
+else
+        bin_directory="$HOME/.distro-config/bin"
 
-export_directory="$HOME/.distro-config/export"
-common_export_file="$export_directory/common-export.sh"
+        export_directory="$HOME/.distro-config/export"
+        common_export_file="$export_directory/common-export.sh"
 
-alias_directory="$HOME/.distro-config/alias"
-common_alias_file="$alias_directory/common-alias.sh"
+        alias_directory="$HOME/.distro-config/alias"
+        common_alias_file="$alias_directory/common-alias.sh"
 
-# If the bin directory exists and is not on the PATH add it
-if [ -d "$bin_directory" ] && ! _is_on_path "$bin_directory"; then
-	PATH="$bin_directory:$PATH"
-fi
-
-if [ -d "$alias_directory" ]; then
-        if [ -e "$common_alias_file" ]; then
-                # shellcheck source=/dev/null
-                . "$common_alias_file"
+        # If the bin directory exists and is not on the PATH add it
+        if [ -d "$bin_directory" ] && ! _is_on_path "$bin_directory"; then
+                PATH="$bin_directory:$PATH"
         fi
 
-        # Keep this after the alias so the common
-        # ones can get overriden by program specific values
-        for alias_file in $(echo "$alias_directory"/**/*alias.sh)
-        do
-                if [ -e "$alias_file" ]; then
+        if [ -d "$alias_directory" ]; then
+                if [ -e "$common_alias_file" ]; then
                         # shellcheck source=/dev/null
-                        . "$alias_file"
+                        . "$common_alias_file"
                 fi
-        done
-fi
 
-if [ -d "$export_directory" ]; then
-        if [ -e "$common_export_file" ]; then
-                # shellcheck source=/dev/null
-                . "$common_export_file"
+                # Keep this after the alias so the common
+                # ones can get overriden by program specific values
+                for alias_file in $(echo "$alias_directory"/**/*alias.sh)
+                do
+                        if [ -e "$alias_file" ]; then
+                                # shellcheck source=/dev/null
+                                . "$alias_file"
+                        fi
+                done
         fi
 
-        # Keep this after the exports so the common
-        # ones can get overriden by program specific values
-        for export_file in $(echo "$export_directory"/**/*export.sh)
-        do
-                if [ -e "$export_file" ]; then
+        if [ -d "$export_directory" ]; then
+                if [ -e "$common_export_file" ]; then
                         # shellcheck source=/dev/null
-                        . "$export_file"
+                        . "$common_export_file"
                 fi
-        done
-fi
 
+                # Keep this after the exports so the common
+                # ones can get overriden by program specific values
+                for export_file in $(echo "$export_directory"/**/*export.sh)
+                do
+                        if [ -e "$export_file" ]; then
+                                # shellcheck source=/dev/null
+                                . "$export_file"
+                        fi
+                done
+        fi
+fi
