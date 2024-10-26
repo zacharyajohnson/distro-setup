e1da7597255af79a06cace1c6c1e15e7530d5665
Zachary Alan Johnson <zjohnson366@protonmail.com>
2024-10-26 10:37:48 -0400

    Update install-scripts.sh variable to script_folder
    This was originally distro_config_bin_folder but since
    we are saving to a folder named /script, it doesn't make
    much sense anymore

--- software/install-scripts.sh
+++ software/install-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 folder=$1
-distro_config_bin_folder="$HOME/.distro-config/script"
+script_folder="$HOME/.distro-config/script"
 
 if [ -z "$HOME" ]; then
         echo "$0: HOME environment variable is not set" >&2
@@ -12,9 +12,9 @@ if [ -z "$folder" ]; then
         echo "$0: Provide folder to look for bin scripts." >&2
         exit 1
 elif [ -d "$folder/script" ]; then
-        echo "Installing scripts for $folder to $distro_config_bin_folder"
-        mkdir -p "$distro_config_bin_folder"
-        cp -r "$folder/script/." "$distro_config_bin_folder"
+        echo "Installing scripts for $folder to $script_folder"
+        mkdir -p "$script_folder"
+        cp -r "$folder/script/." "$script_folder"
 else
         echo "Script folder does not exist for $folder. Skipping..."
 fi
