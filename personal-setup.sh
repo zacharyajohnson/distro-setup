60e881acf68179a296636659e4ac46e5dcf2db22
Zachary Alan Johnson <zjohnson366@protonmail.com>
2025-10-13 13:50:47 -0400

    Add bluez and bluz-utils software folders

--- personal-setup.sh
+++ personal-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-software=anki,asunder,cron,discord,flatpak,git,intellij,keepassxc,kid3,lgog-downloader\
+software=anki,asunder,bluez,bluez-utils,cron,discord,flatpak,git,intellij,keepassxc,kid3,lgog-downloader\
 ,mgba,nautilus-dropbox,proprietary-nvidia-driver,psensor,qbittorrent\
 ,qdirstat,rustlang,shellcheck,steam,tmux,vim,wine,xfce
 
