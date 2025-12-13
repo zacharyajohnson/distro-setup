4a9dfe88fb93b91edecd3dd15160b1fc9d2d49ae
Zachary Alan Johnson <zjohnson366@protonmail.com>
2025-12-13 14:31:08 -0500

    Remove undefined software_option_values variable

--- software/install-non-native-package-managers.sh
+++ software/install-non-native-package-managers.sh
@@ -12,7 +12,7 @@ non_native_package_managers='flatpak brew'
 for non_native_package_manager in $non_native_package_managers
 do
         folder="$dirname/$non_native_package_manager"
-        if ! "$dirname/install-software-and-files.sh" "$folder" "$native_package_manager" "$software_option_values"; then
+        if ! "$dirname/install-software-and-files.sh" "$folder" "$native_package_manager"; then
                 echo "$0: Setup failed for $non_native_package_manager" >&2
                 exit 1
         fi
