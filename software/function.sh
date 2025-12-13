5e70671942469fd9867c3595ebaf90c4c686c105
Zachary Alan Johnson <zjohnson366@protonmail.com>
2025-12-13 16:38:33 -0500

    Change logic to detect package manager to POSIX command -v
    As part of the software installation process, we have to detect
    which package manager is installed so we can install the software
    selected.
    
     We were using which $package_manager, but that is not POSIX compliant.
    
    Command -v does the same thing and is POSIX compliant so we are using
    this from now on.

--- software/function.sh
+++ software/function.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 _get_native_package_manager() {
-        if which dpkg > '/dev/null' 2>&1; then
+        if command -v dpkg > '/dev/null' 2>&1; then
                 printf 'dpkg'
                 return 0
-        elif which pkg > '/dev/null' 2>&1; then
+        elif command -v pkg > '/dev/null' 2>&1; then
                 printf 'pkg'
                 return 0
-        elif which pacman > '/dev/null' 2>&1; then
+        elif command -v pacman > '/dev/null' 2>&1; then
                 printf 'pacman'
                 return 0
-        elif which apk > '/dev/null' 2>&1; then
+        elif command -v apk > '/dev/null' 2>&1; then
                 printf 'apk'
                 return 0
         else
@@ -19,10 +19,10 @@ _get_native_package_manager() {
 }
 
 _get_non_native_package_manager() {
-        if which flatpak > '/dev/null' 2>&1; then
+        if command -v flatpak > '/dev/null' 2>&1; then
                 printf 'flatpak'
                 return 0
-        elif which brew > '/dev/null' 2>&1; then
+        elif command -v brew > '/dev/null' 2>&1; then
                 printf 'brew'
                 return 0
         else
