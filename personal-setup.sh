d9b4bc81d5e1d25dbcb7e646ddbffd6f219a880b
Zachary Alan Johnson <zjohnson366@protonmail.com>
2026-06-01 15:44:29 -0400

    Add xorg-server to personal-setup.sh

--- personal-setup.sh
+++ personal-setup.sh
@@ -2,6 +2,6 @@
 
 software=anki,asunder,calibre,cron,discord,firefox,git,intellij,keepassxc,kid3,lgog-downloader\
 ,mullvad,nautilus-dropbox,nsxiv,openrgb,proprietary-nvidia-driver,psensor,qbittorrent\
-,qdirstat,shellcheck,smartmontools,steam,tmux,vim,wine,xfce
+,qdirstat,shellcheck,smartmontools,steam,tmux,vim,wine,xfce,xorg-server
 
 "$(dirname "$0")"'/setup.sh' --software="$software" --prompt-for-install --native-and-non-native-packages
