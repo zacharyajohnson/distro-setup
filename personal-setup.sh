433dbdf3dd33a3373678c38d1ac0c898272f736b
Zachary Alan Johnson <zjohnson366@protonmail.com>
2026-06-01 15:05:25 -0400

    Add smartmontools to personal.setup.sh

--- personal-setup.sh
+++ personal-setup.sh
@@ -2,6 +2,6 @@
 
 software=anki,asunder,cron,discord,git,intellij,keepassxc,kid3,lgog-downloader\
 ,nautilus-dropbox,proprietary-nvidia-driver,psensor,qbittorrent\
-,qdirstat,shellcheck,steam,tmux,vim,wine,xfce
+,qdirstat,shellcheck,smartmontools,steam,tmux,vim,wine,xfce
 
 "$(dirname "$0")"'/setup.sh' --software="$software" --prompt-for-install --native-and-non-native-packages
