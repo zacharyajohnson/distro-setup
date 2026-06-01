e72c7f1dc223ac9f695dcd1c87ec10a2a2853bb0
Zachary Alan Johnson <zjohnson366@protonmail.com>
2026-06-01 15:48:40 -0400

    Add jq to personal-setup.sh

--- personal-setup.sh
+++ personal-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-software=anki,asunder,calibre,cron,discord,firefox,git,intellij,keepassxc,kid3,lgog-downloader\
+software=anki,asunder,calibre,cron,discord,firefox,git,intellij,jq,keepassxc,kid3,lgog-downloader\
 ,mullvad,nautilus-dropbox,nsxiv,openrgb,proprietary-nvidia-driver,psensor,qbittorrent\
 ,qdirstat,shellcheck,smartmontools,steam,tmux,vim,wine,xfce,xorg-server
 
