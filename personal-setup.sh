0be59061ea6147e3a41d035cdda02e1b1535266e
Zachary Alan Johnson <zjohnson366@protonmail.com>
2026-06-08 00:46:12 -0400

    Add unzip to personal-setup.sh

--- personal-setup.sh
+++ personal-setup.sh
@@ -2,6 +2,6 @@
 
 package=anki,asunder,calibre,cron,discord,firefox,git,jetbrains-toolbox,jq,keepassxc,kid3,lgog-downloader\
 ,mullvad,nautilus-dropbox,noto-fonts,nsxiv,openrgb,proprietary-nvidia-driver,psensor,qbittorrent\
-,qdirstat,shellcheck,smartmontools,steam,tmux,vim,wine,xfce,xorg-server
+,qdirstat,shellcheck,smartmontools,steam,tmux,unzip,vim,wine,xfce,xorg-server
 
 "$(dirname "$0")"'/setup.sh' --package="$package" --prompt-for-install --native-and-non-native-packages
